478f2c60f594383a776f931b17c6838539d06053
Terence Holmes <tdholmes2@agora.cs.wcu.edu>
2018-04-16 17:25:20 -0400

    Merge about to happen

--- src/Machine.java
+++ src/Machine.java
@@ -18,41 +18,40 @@ import java.util.InputMismatchException;
  * @Author - Melchor Dominguez, Terence Holmes
  * Machine Class that will make a finite state machine
  */
-public class Machine{ 
+public class Machine{
 
     /**
      * Main functon that starts and collects results from all thread
      */
     public static void main(String[] args){
 
+        /** int that holds number of finite state machines */
         int fsm = 0;
-
+        /** int that holds number of iterations to perform */
         int iterations = 0;
-
+        /** int that holds number of threads */
         int threads = 0;
-
+        /** String that holds file name */
         String filename = "";
-
+        /** File holding finite state machine info */
         File file = null;
-
+        
         /** Scanner to get input from keyboard*/
         Scanner sc = new Scanner(System.in);
         try{
         //Prompt user for how many machines to create
         System.out.print("How many Finite State Machines to create? > ");
-        /** int that holds number of finite state machines */
         fsm = sc.nextInt();
         
         //Prompt user for how many iterations to perform
         System.out.print("How many iterations for each machine? > ");
-        /** int that holds number of iterations*/
         iterations = sc.nextInt();
         
         //Prompt user for how many threads to create
         System.out.print("How many threads? > ");
-        /** int that holds number of threads*/
         threads = sc.nextInt();
         }
+        
         catch(InputMismatchException ime){
             System.out.println("Please ensure that input is an integer!");
             System.exit(1);
@@ -62,7 +61,6 @@ public class Machine{
             System.exit(1);
         }
         System.out.print("Please enter input fileName > ");
-        /** String that holds file name */
         filename = sc.next();
         file = new File(filename);
         if(!file.exists() || file.isDirectory()){
@@ -71,26 +69,25 @@ public class Machine{
         }
         // Initialize Data for the finite state machines to use
         Data data = new Data(file);
+        
+        final ExecutorCompletionService<Data> complete = poolParty(threads, 
+                            data, file, iterations, fsm, args);
+        finalize(data, fsm, complete);
 
+        }//end main
         
-        /** Threadpool to have all the threads*/
-        final ExecutorService pool = Executors.newFixedThreadPool(threads);
-        /** */
-        final ExecutorCompletionService<Data> complete = 
-                                                new ExecutorCompletionService<Data>(pool);
-        int startState;
-        if(args.length < 2){
-            Random ran = new Random();
-            startState = ran.nextInt(data.getSize() - 1);
-        }else{
-            startState = Integer.parseInt(args[1]);
-        }
-        
-        //try{
-        //    queue.put(data);
-       // }catch(InterruptedException e){
-        //    e.printStackTrace();
-        //}
+        private static ExecutorCompletionService<Data> poolParty(int threads, Data data, File file, int iterations, int fsm, String[] args){
+            /** Threadpool to have all the threads*/
+            final ExecutorService pool = Executors.newFixedThreadPool(threads);
+            /** */
+            ExecutorCompletionService<Data> complete =  new ExecutorCompletionService<Data>(pool);
+            int startState;
+            if(args.length < 2){
+                Random ran = new Random();
+                startState = ran.nextInt(data.getSize() - 1);
+            }else{
+                startState = Integer.parseInt(args[1]);
+            }
 
         for(int i = 0; i < fsm; i++){
             data = new Data(file);
@@ -98,10 +95,14 @@ public class Machine{
         }//end for
         
         pool.shutdown();
-        float[] finalResults = new float[data.getSize()];
-        float total = 0;
+        return complete;
+        }
 
-        for(int i = 0; i < fsm; i++){
+        private static void finalize(Data data, int fsm, ExecutorCompletionService<Data> complete){
+            float[] finalResults = new float[data.getSize()];
+            float total = 0;
+
+            for(int i = 0; i < fsm; i++){
             try{
                 final Future<Data> future = complete.take();
                 final Data curData = future.get();
@@ -122,6 +123,5 @@ public class Machine{
         for(int i = 0; i < finalResults.length; i++){
             System.out.println("-State " + i + ": " + finalResults[i]/total);
         }//for 
-
     }//end machine
 }//end Machine class
